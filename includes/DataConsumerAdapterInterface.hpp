d6d3aea374ede60d0841fcec8e6ef0e4a3079bb1
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-09-21 12:25:10 +0200

    apply conan rework

--- includes/DataConsumerAdapterInterface.hpp
+++ includes/DataConsumerAdapterInterface.hpp
@@ -2,60 +2,55 @@
 #define __DATA_CONSUMER_ADAPTER_INTERFACE_HPP_
 
 #include "Listener.hpp"
+#include "LoggerRepository.hpp"
+#include "Stoppable.hpp"
 
 /**
  * @brief Data Consumer Adapter Interface
- * This namespace containes all of the information, required by the Information Model Manager. 
+ * This namespace containes all of the information, required by the Information
+ * Model Manager.
  * @author Dovydas Girdvainis
  * @date 04.12.2019
  */
 namespace DCAI {
-  /**
-   * @brief State enum for the Data Consumer Adapter implementations
-   * 
-   */
-  typedef enum DataConsumerAdapterStatusEnum {
-    INITIALISING, /* Set in DataConsumerAdapterInterface::start() routine as its initial step. During this state all of the necessary configuration routines are executed */
-    RUNNING, /* Set in DataConsumerAdapterInterface::start() routine as its final step, if it succeded. During this state the adapter is interacting with the Information Model Manager */
-    STOPPED, /* Set in DataConsumerAdapterInterface::stop(), if it succeded. During this state, adapter stops all of the interactions with the Information Model Manager */
-    EXITED, /* Set in DataConsumerAdapterInterface::start() routine as its final step, if it failed. During this state, adapter stops all of the interactions with the Information Model Manager*/
-    UNKNOWN /* Initial state, before DataConsumerAdapterInterface::start() routine is called. During this state, adapter does not interact with the Infromation Model Manager */
-  } DataConsumerAdapterStatus;
+/**
+ * @brief Generic Interface for all Data Consumer Adapter Implementations
+ *
+ */
+class DataConsumerAdapterInterface : public Model_Event_Handler::Listener,
+                                     public Stoppable {
 
-  /**
-   * @brief Generic Interface for all Data Consumer Adapter Implementations
-   * 
-   */
-  class DataConsumerAdapterInterface : public Model_Event_Handler::Listener {
-   public:
-    /**
-     * @brief Starts the adapter. 
-     * 
-     * If succeded, sets the internal state to DataConsumerAdapterStatus::RUNNING
-     * 
-     */
-    virtual void start() = 0;
-    /**
-     * @brief Stops the adapter.
-     * 
-     * If succeded, sets the internal state to DataConsumerAdapterStatus::STOPPED
-     */
-    virtual void stop() = 0;
-    /**
-     * @brief Gets the current state of the adapter
-     * 
-     * @return DataConsumerAdapterStatus 
-     */
-    virtual DataConsumerAdapterStatus getStatus() = 0;
-    /**
-     * @brief Handles incoming event from Information Model Manager
-     * 
-     * @param event 
-     */
-    virtual void handleEvent(Model_Event_Handler::NotifierEvent* event) = 0;
+  const std::string adapter_name_;
 
-    ~DataConsumerAdapterInterface() = default;
-  };
-}   // namespace DCAI
+protected:
+  std::shared_ptr<HaSLL::Logger> adapter_logger_;
 
-#endif   //__DATA_CONSUMER_ADAPTER_INTERFACE_HPP_
+public:
+  DataConsumerAdapterInterface(const std::string &name)
+      : adapter_name_(name),
+        adapter_logger_(HaSLL::LoggerRepository::getInstance().registerLoger(
+            adapter_name_)) {
+    adapter_logger_->log(
+        HaSLL::SeverityLevel::TRACE,
+        "DataConsumerAdapterInterface::DataConsumerAdapterInterface({})",
+        adapter_name_);
+  }
+
+  ~DataConsumerAdapterInterface() = default;
+
+  const std::string getAdapterName() const { return adapter_name_; }
+
+  void start() {
+    adapter_logger_->log(HaSLL::SeverityLevel::INFO, "Started!");
+    Stoppable::start();
+  }
+
+  void stop() {
+    adapter_logger_->log(HaSLL::SeverityLevel::INFO,
+                         "Received a stop command!");
+    Stoppable::stop();
+  }
+};
+} // namespace DCAI
+
+#endif //__DATA_CONSUMER_ADAPTER_INTERFACE_HPP_
---
+++ unit_tests/Data_Consumer_Adapter_Interface_Tests/data_consumer_adapter_interface_tests.cc
@@ -0,0 +1,33 @@
+#include "DataConsumerAdapterInterface_MOCK.hpp"
+#include "Device_MOCK.hpp"
+#include "Notifier.hpp"
+
+#include <gtest/gtest.h>
+#include <memory>
+
+using namespace std;
+using namespace DCAI::testing;
+using namespace Information_Model::testing;
+using namespace Model_Event_Handler;
+
+TEST(DCAI_Test, canHandleEvent) {
+  shared_ptr<Listener> adapter =
+      make_shared<DataConsumerAdapterInterfaceMock>("adopt me!");
+  auto notifier = make_unique<Notifier>();
+  notifier->registerListener(adapter);
+
+  auto event = make_shared<NotifierEvent>(
+      NotifierEventType::NEW_DEVICE_REGISTERED,
+      make_shared<MockDevice>("12345", "Mock", "Mock device"));
+  EXPECT_CALL(*(static_pointer_cast<DataConsumerAdapterInterfaceMock>(adapter)),
+              handleEvent(event));
+
+  notifier->notifyListeners(event);
+}
+
+TEST(DCAI_Test, canStart) {
+  DataConsumerAdapterInterfaceMock adapter("start me!");
+  EXPECT_CALL(adapter, run());
+
+  adapter.start();
+}
