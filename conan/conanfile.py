aa662f0d003d560a8e4f0ed9a877e29a53623983
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-09-28 16:38:16 +0200

    bump Model Event Handler to v0.1.1

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -11,7 +11,7 @@ class PackageConan(ConanFile):
     build_requires = "gtest/1.10.0"
     requires = [
         "HaSLL/0.2.2@hahn-schickard/stable",
-        "Model_Event_Handler/0.1.0@hahn-schickard/stable"
+        "Model_Event_Handler/0.1.1@hahn-schickard/stable"
     ]
     settings = "cppstd", "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
