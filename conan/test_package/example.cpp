8f4eadcc86cd1e23a2cf6be5f1962596c9de3660
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2020-10-09 18:03:13 +0200

    fix integration test

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -6,15 +6,21 @@
 using namespace std;
 using namespace DCAI;
 
+class EventSourceFake : public Event_Model::EventSource<ModelRegistryEvent> {
+public:
+  void sendEvent(std::shared_ptr<ModelRegistryEvent> event) { notify(event); }
+};
+
 class Fake_DCAI : public DataConsumerAdapterInterface {
-  void
-  handleEvent(shared_ptr<Model_Event_Handler::NotifierEvent> event) override {
+  void handleEvent(shared_ptr<ModelRegistryEvent> event) override {
     cout << "Received an event! " << endl;
   }
 
 public:
-  Fake_DCAI()
-      : DataConsumerAdapterInterface("Fake Data Consumer Adapter Interface") {}
+  Fake_DCAI(
+      Event_Model::EventSourceInterfacePtr<ModelRegistryEvent> event_source)
+      : DataConsumerAdapterInterface("Fake Data Consumer Adapter Interface",
+                                     event_source) {}
 };
 
 int main() {
@@ -22,7 +28,7 @@ int main() {
       "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
       HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
   HaSLL::LoggerRepository::initialise(config);
-  auto adapter = Fake_DCAI();
+  auto adapter = Fake_DCAI(make_shared<EventSourceFake>());
   cout << adapter.getAdapterName() << endl;
   exit(EXIT_SUCCESS);
 }
