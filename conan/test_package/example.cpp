6bcafd9fed6ba2785e8f8f02cdd67b372fef4450
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2021-03-22 11:26:06 +0100

    update conan integration test

--- conan/test_package/example.cpp
+++ conan/test_package/example.cpp
@@ -1,26 +1,55 @@
-#include "DataConsumerAdapterInterface.hpp"
+#include "Data_Consumer_Adapter_Interface/DataConsumerAdapterInterface.hpp"
+#include "Event_Model.hpp"
+#include "Information_Model/mocks/DeviceMockBuilder.hpp"
 #include "LoggerRepository.hpp"
+#include "Variant_Visitor.hpp"
 
 #include <iostream>
 
 using namespace std;
 using namespace DCAI;
+using namespace HaSLL;
+using namespace Information_Model;
+
+struct DCAI_Example : DataConsumerAdapterInterface {
+  DCAI_Example(ModelEventSourcePtr source)
+      : DataConsumerAdapterInterface(source, "Example DCAI") {}
+
+  void start() override final {
+    cout << getAdapterName() << " Started!" << endl;
+    DataConsumerAdapterInterface::start();
+  }
+
+  void stop() override final {
+    cout << getAdapterName() << " Received a stop command!" << endl;
+    DataConsumerAdapterInterface::stop();
+  }
+
+private:
+  void handleEvent(shared_ptr<ModelRegistryEvent> event) {
+    this->logger_->log(SeverityLevel::TRACE, "Received an event!");
+    match(*event.get(),
+          [&](string identifier) {
+            cout << "Device: " << identifier << " was deregistered!" << endl;
+          },
+          [&](DevicePtr device) {
+            cout << "Device: " << device->getElementName() << ":"
+                 << device->getElementId() << " was registered!" << endl;
+          });
+  }
+};
 
 class EventSourceFake : public Event_Model::EventSource<ModelRegistryEvent> {
 public:
-  void sendEvent(std::shared_ptr<ModelRegistryEvent> event) { notify(event); }
-};
-
-class Fake_DCAI : public DataConsumerAdapterInterface {
-  void handleEvent(shared_ptr<ModelRegistryEvent> event) override {
-    cout << "Received an event! " << endl;
+  void registerDevice(DevicePtr device) {
+    auto event = make_shared<ModelRegistryEvent>(device);
+    notify(move(event));
   }
 
-public:
-  Fake_DCAI(
-      Event_Model::EventSourceInterfacePtr<ModelRegistryEvent> event_source)
-      : DataConsumerAdapterInterface("Fake Data Consumer Adapter Interface",
-                                     event_source) {}
+  void deregisterDevice(string identifier) {
+    auto event = make_shared<ModelRegistryEvent>(identifier);
+    notify(move(event));
+  }
 };
 
 int main() {
@@ -28,7 +57,24 @@ int main() {
       "./log", "logfile.log", "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
       HaSLL::SeverityLevel::TRACE, false, 8192, 2, 25, 100, 1);
   HaSLL::LoggerRepository::initialise(config);
-  auto adapter = Fake_DCAI(make_shared<EventSourceFake>());
-  cout << adapter.getAdapterName() << endl;
-  exit(EXIT_SUCCESS);
+
+  auto event_source = make_shared<EventSourceFake>();
+  auto dcai = DCAI_Example(event_source);
+
+  dcai.start();
+
+  string device_id = "1234";
+  {
+    auto builder = new Information_Model::testing::DeviceMockBuilder();
+    builder->buildDeviceBase(device_id, "Mocky",
+                             "A mocked device with no elements");
+    event_source->registerDevice(builder->getResult());
+    delete builder;
+  }
+
+  event_source->deregisterDevice(device_id);
+
+  dcai.stop();
+
+  return EXIT_SUCCESS;
 }
