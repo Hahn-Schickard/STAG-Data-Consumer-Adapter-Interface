0a135c0055130d6253f6432169fb394542741988
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2023-12-11 13:43:04 +0100

    simplify integration test

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,8 +1,8 @@
 #include "Data_Consumer_Adapter_Interface/DataConsumerAdapterInterface.hpp"
+
 #include "Event_Model/AsyncEventSource.hpp"
 #include "HaSLL/LoggerManager.hpp"
 #include "HaSLL/SPD_LoggerRepository.hpp"
-#include "Information_Model/mocks/DeviceMockBuilder.hpp"
 #include "Variant_Visitor.hpp"
 
 #include <exception>
@@ -13,44 +13,7 @@ using namespace std;
 using namespace Data_Consumer_Adapter;
 using namespace HaSLL;
 using namespace Information_Model;
-
-// NOLINTNEXTLINE(readability-identifier-naming)
-struct DCAI_Example : DataConsumerAdapterInterface {
-  DCAI_Example(ModelEventSourcePtr source)
-      /* Never move into Model Event Source ptr either! */
-      : DataConsumerAdapterInterface(source, "Example DCAI") {} // NOLINT
-
-  void start(std::vector<Information_Model::DevicePtr> devices = {}) final {
-    logger->log(SeverityLevel::TRACE, "{} Started!", name);
-    DataConsumerAdapterInterface::start(devices);
-  }
-
-private:
-  void registrate(Information_Model::NonemptyDevicePtr device) override {
-    auto emplaced = devices_.emplace(device->getElementId());
-    if (emplaced.second) {
-      logger->log(SeverityLevel::TRACE, "Device: {} was registered!",
-          device->getElementName());
-    } else {
-      logger->log(SeverityLevel::TRACE,
-          "Device: {} was already registered. Ignoring new instance!",
-          device->getElementName());
-    }
-  }
-
-  void deregistrate(const std::string& device_id) override {
-    auto it = devices_.find(device_id);
-    if (it != devices_.end()) {
-      logger->log(
-          SeverityLevel::TRACE, "Device: {} was deregistered!", device_id);
-    } else {
-      string error_msg = "Device " + device_id + " does not exist!";
-      throw runtime_error(error_msg);
-    }
-  }
-
-  set<string> devices_;
-};
+using namespace Event_Model;
 
 void printException(const exception& e, int level = 0) {
   cerr << string(level, ' ') << "Exception: " << e.what() << endl;
@@ -62,80 +25,29 @@ void printException(const exception& e, int level = 0) {
   }
 }
 
-class EventSourceFake
-    : public Event_Model::AsyncEventSource<ModelRepositoryEvent> {
-
-  LoggerPtr logger_;
-
-  void handleException(std::exception_ptr ex_ptr) { // NOLINT
-    try {
-      if (ex_ptr) {
-        rethrow_exception(ex_ptr);
-      }
-    } catch (const exception& ex) {
-      logger_->log(SeverityLevel::ERROR,
-          "An exception occurred while notifying a listener: {}", ex.what());
-      printException(ex);
-    }
-  }
-
-public:
-  EventSourceFake()
-      : AsyncEventSource(
-            bind(&EventSourceFake::handleException, this, placeholders::_1)),
-        logger_(LoggerManager::registerTypedLogger(this)) {}
-
-  void registerDevice(NonemptyDevicePtr device) {
-    auto event = std::make_shared<ModelRepositoryEvent>(device);
-    logger_->log(SeverityLevel::TRACE,
-        "Notifing listeners that Device {} is available for registration.",
-        device->getElementId());
-    notify(move(event));
-  }
-
-  void deregisterDevice(string identifier) {
-    auto event = make_shared<ModelRepositoryEvent>(identifier);
-    logger_->log(SeverityLevel::TRACE,
-        "Notifing listeners that Device {} is no longer available", identifier);
-    notify(move(event));
-  }
-};
-
 int main() {
   try {
-    auto config = HaSLL::SPD_Configuration("./log", "logfile.log",
-        "[%Y-%m-%d-%H:%M:%S:%F %z][%n]%^[%l]: %v%$",
-        HaSLL::SeverityLevel::TRACE, true, 8192, 2, 25, 100, 1); // NOLINT
-    auto repo = make_shared<SPD_LoggerRepository>(config);
+    auto repo = make_shared<SPD_LoggerRepository>();
     LoggerManager::initialise(repo);
 
-    auto event_source = make_shared<EventSourceFake>();
-    auto dcai = DCAI_Example(event_source);
+    auto event_source =
+        std::make_shared<AsyncEventSource<ModelRepositoryEvent>>(
+            [](exception_ptr ex_ptr) {
+              try {
+                if (ex_ptr) {
+                  rethrow_exception(ex_ptr);
+                }
+              } catch (const exception& ex) {
+                printException(ex);
+              }
+            });
+    auto dcai =
+        std::make_shared<DataConsumerAdapterInterface>(event_source, "example");
 
-    dcai.start();
-
-    string device_id = "1234";
-    {
-      auto* builder = new Information_Model::testing::DeviceMockBuilder();
-      builder->buildDeviceBase(
-          device_id, "Mocky", "A mocked device with no elements");
-      NonemptyDevicePtr result(builder->getResult());
-      event_source->registerDevice(result);
-      event_source->registerDevice(
-          result); // check if double registration is handled
-      delete builder;
-    }
-
-    event_source->deregisterDevice(device_id);
-    event_source->deregisterDevice("nonsense ID"); // check bad ID deregister
-
-    this_thread::sleep_for(2s);
-
-    dcai.stop();
-
-    return EXIT_SUCCESS;
   } catch (const exception& ex) {
     printException(ex);
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
+
+  exit(EXIT_SUCCESS);
 }
