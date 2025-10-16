b81e1c5876f07580652a46c5a343c7660449aa6f
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-10-16 13:14:20 +0200

    refactor test package to only test symbols

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,58 +1,20 @@
-#include "Data_Consumer_Adapter_Interface/DataConsumerAdapterInterface.hpp"
+#include <Data_Consumer_Adapter_Interface/DataConsumerAdapter.hpp>
+#include <HaSLL/LoggerManager.hpp>
 
-#include "Event_Model/AsyncEventSource.hpp"
-#include "HaSLL/LoggerManager.hpp"
-#include "Variant_Visitor.hpp"
-
-#include <exception>
 #include <iostream>
-#include <set>
 
 using namespace std;
-using namespace Data_Consumer_Adapter;
-using namespace HaSLL;
-using namespace Information_Model;
-using namespace Event_Model;
-
-void printException(const exception& e, int level = 0) {
-  cerr << string(level, ' ') << "Exception: " << e.what() << endl;
-  try {
-    rethrow_if_nested(e);
-  } catch (const exception& nested_exception) {
-    printException(nested_exception, level + 1);
-  } catch (...) {
-  }
-}
 
 int main() {
-  auto status = EXIT_SUCCESS;
-  try {
-    LoggerManager::initialise(makeDefaultRepository());
+  Information_Model::DevicePtr device;
+  Data_Consumer_Adapter::ModelRepositoryEventPtr event;
+  Data_Consumer_Adapter::DataConnectionPtr connection;
+  Data_Consumer_Adapter::DataNotifier notifier;
+  Data_Consumer_Adapter::DataConnector connector;
+  Data_Consumer_Adapter::DataConsumerAdapterPtr adapter;
+  HaSLL::LoggerPtr logger;
 
-    try {
-      auto event_source =
-          std::make_shared<AsyncEventSource<ModelRepositoryEvent>>(
-              [](exception_ptr ex_ptr) {
-                try {
-                  if (ex_ptr) {
-                    rethrow_exception(ex_ptr);
-                  }
-                } catch (const exception& ex) {
-                  printException(ex);
-                }
-              });
-      auto dcai = std::make_shared<DataConsumerAdapterInterface>(
-          event_source, "example");
+  cout << "Integration Test successful" << endl;
 
-    } catch (const exception& ex) {
-      printException(ex);
-      status = EXIT_FAILURE;
-    }
-    LoggerManager::terminate();
-  } catch (...) {
-    cerr << "Unknown error occurred during program execution." << endl;
-    status = EXIT_FAILURE;
-  }
-
-  exit(status);
+  return 0;
 }
