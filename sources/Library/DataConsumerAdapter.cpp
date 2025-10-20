ea3616ccc0978dea9b55b00aa407c1ec540522b0
Dovydas Girdvainis <dovydas.girdvainis@hahn-schickard.de>
2025-10-20 12:32:53 +0200

    add trace level logging

--- sources/Library/DataConsumerAdapter.cpp
+++ sources/Library/DataConsumerAdapter.cpp
@@ -3,9 +3,6 @@
 #include <HaSLL/LoggerManager.hpp>
 #include <Variant_Visitor/Visitor.hpp>
 
-#include <stdexcept>
-#include <thread>
-
 namespace Data_Consumer_Adapter {
 using namespace std;
 using namespace Information_Model;
@@ -20,12 +17,13 @@ DataConsumerAdapter::DataConsumerAdapter(
 
 string DataConsumerAdapter::name() const { return name_; }
 
-void DataConsumerAdapter::start() { logger->info("Started!"); }
+void DataConsumerAdapter::start() { logger->info("Started"); }
 
 void DataConsumerAdapter::stop() { logger->info("Stopped"); }
 
 void DataConsumerAdapter::initialiseModel(const Devices& devices) {
   auto event_lock = scoped_lock(mx_);
+  logger->trace("Initialising Data Consumer Adapter");
   for (const auto& device : devices) {
     registerDevice(device);
   }
@@ -36,11 +34,12 @@ void DataConsumerAdapter::handleEvent(const RegistryChangePtr& event) {
   Variant_Visitor::match(
       *event,
       [this](const DevicePtr& device) {
-        auto registrate_lock = scoped_lock(mx_);
+        logger->trace("Registering a new device {}", device->id());
         registerDevice(device);
       },
       [this](const string& device_id) {
         try {
+          logger->trace("Removing device {}", device_id);
           deregistrate(device_id);
         } catch (const exception& ex) {
           logger->error(
